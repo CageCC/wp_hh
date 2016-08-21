c8d24a04b710bbae73365a17ea5b6a890e952a1e
CageCC <cage2cc@gmail.com>
2016-08-21 15:09:39 +0800

    Revert "# add"
    This reverts commit 64b1e0f1f5212917d725994df7abc97bb7cfc30f.

--- wordpress/wp-content/themes/medicenter-child/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-function child_theme_enqueue_styles()
-{
-	wp_enqueue_style("parent-style", get_template_directory_uri() . "/style.css", array("reset", "superfish", "jquery-fancybox", "jquery-qtip", "jquery-ui-custom"));
-}
-add_action("wp_enqueue_scripts", "child_theme_enqueue_styles");
